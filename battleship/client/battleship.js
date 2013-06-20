208a34f44dd0fdccc48121c07b0aa9cb7768f44d
Vanessa <me@vanessa-lyn.com>
2013-06-19 17:31:22 -0700

    updating with data added to the db. may need to revert back to prototyping until all the major functionality is built. but commiting this to I have code to review when storing actual data.

--- battleship/client/battleship.js
+++ battleship/client/battleship.js
@@ -1,42 +1,32 @@
-var games = [
-  {gameId: 'COD', currentPlayers: "Soap vs. Price", gameUrl: '/battle/cod', 
-    war:[
-      {player: 'Soap', score: 10},
-      {player: 'Price', score: 3},
-    ]},
-  {gameId: 'Battlefield 3', currentPlayers: "Henry Blackburn vs. Solomon", gameUrl: '/battle/battlefield'},
-  {gameId: 'Some other FPS game', currentPlayers: "thisGuy vs. thatGuy", gameUrl: '/battle/fps'}
-];
-
-var players = [
-  {userName: 'ME', status: "..."},
-  {userName: 'Soap', status: "Handling Fools"},
-  {userName: 'Price', status: "hatin'"},
-  {userName: 'random666', status: "READY"},
-  {userName: 'Henry Blackburn', status: "Noob Toobin'"},
-  {userName: 'yoMama', status: "READY"},
-  {userName: 'Solomon', status: "Battling"},
-  {userName: 'thisGuy', status: "Relieving Stress"},
-  {userName: 'Dude', status: "READY"},
-  {userName: 'dudette', status: "READY"},
-  {userName: 'thatGuy', status: "Lighting up some beatches"},
-];
- 
-Template.games.helpers({
-  games: games
-});
-
-Template.players.helpers({
-  players: players
-});
-
-Template.gameTemplate.helpers({
-  gameTemplate: games[0]
-});
 
 
+//Router Setup
 Meteor.Router.add({
   '/': 'homeTemplate',
   '/user/:id': 'profileTemplate',
   '/battle/:id': 'gameTemplate'
 });
+
+
+/////////////////////////////
+//STATIC DATA: Prototyping phase 
+
+/////////////////////////////
+
+
+//helpers to pull in the data into the app
+Template.games.helpers({
+  games: function(){ return Games.find(); }
+});
+
+Template.players.helpers({
+  players: function (){ return Players.find(); }
+});
+
+
+Template.gameTemplate.helpers({
+  gameTemplate: function(){ return Games.findOne(Sessions.get('gameId')); }
+});
+
+
+
---
+++ battleship/lib/collections.js
@@ -0,0 +1,45 @@
+Games = new Meteor.Collection('games');
+Players = new Meteor.Collection('players');
+
+if(Meteor.isServer && Games.find().count() == 0) {
+    var games = [
+      {gameId: 'COD', currentPlayers: "Soap vs. Price", gameUrl: '/battle/cod', 
+        war:[
+          {player: 'Soap', score: 10},
+          {player: 'Price', score: 3},
+        ]},
+      {gameId: 'Battlefield', currentPlayers: "Henry Blackburn vs. Solomon"},
+      {gameId: 'Some other FPS game', currentPlayers: "thisGuy vs. thatGuy"}
+    ];
+
+    //Inserting each of the game rooms into the data object
+    _.each(games, function(game){
+        Games.insert(game);
+    });
+
+}
+
+
+if(Meteor.isServer && Players.find().count() == 0) {
+    
+    var players = [
+      {userName: 'ME', status: "..."},
+      {userName: 'Soap', status: "Handling Fools"},
+      {userName: 'Price', status: "hatin'"},
+      {userName: 'random666', status: "READY"},
+      {userName: 'Henry Blackburn', status: "Noob Toobin'"},
+      {userName: 'yoMama', status: "READY"},
+      {userName: 'Solomon', status: "Battling"},
+      {userName: 'thisGuy', status: "Relieving Stress"},
+      {userName: 'Dude', status: "READY"},
+      {userName: 'dudette', status: "READY"},
+      {userName: 'thatGuy', status: "Lighting up some beatches"},
+    ];
+
+
+    //Inserting each of the game rooms into the data object
+    _.each(players, function(player){
+        Players.insert(player);
+    });
+
+}
